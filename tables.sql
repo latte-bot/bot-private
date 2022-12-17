93e06b700e3afe1e69b6b157f2e2e527a7fc87e0
xSTACIA <6505001948@rumail.ru.ac.th>
2022-12-17 12:06:57 +0700

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS riot_accounts(
-    user_id BIGINT NOT NULL,
+    user_id BIGINT PRIMARY KEY,
     guild_id BIGINT,
     extras VARCHAR,
     date_signed TIMESTAMP,
-    locale VARCHAR
+    locale VARCHAR(100));
 );
